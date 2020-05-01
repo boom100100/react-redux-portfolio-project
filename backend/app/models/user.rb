8d36e5596a5e6d0ba2352011cfc26dfd6cdfd1d1
boom100100 <bsd231@nyu.edu>
2020-05-01 14:46:40 -0400

    Added model, controller, controller CRUD skeleton for user.

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :projects
+  #unnecessary to do has_many :, through: : relationships
+  #but they are obvious relationships to include here
 end
