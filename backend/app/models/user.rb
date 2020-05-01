769d31fc8347178b5de82869a4d8ccc2c03d8562
boom100100 <bsd231@nyu.edu>
2020-05-01 15:01:30 -0400

    Added validations to user.

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   has_many :projects
   #unnecessary to do has_many :, through: : relationships
   #but they are obvious relationships to include here
+
+  validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }#, on: :create
+  validates :password, presence: true
+  validates :password, confirmation: true
 end
