d4193ba0097d0a3aaf85d7056cb12e5806e15e77
boom100100 <bsd231@nyu.edu>
2020-05-01 16:52:08 -0400

    Added user CRUD. Included type in renderings.

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,17 +1,63 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    if @users
+      render :json => @users, include: [:projects => {:include => [:section_titles, :graphs, :data => {:only => [:name, :type, :url, :description, :content]}]}]
+    else
+      render json: {
+        status: 500,
+        message: 'No users found.'
+      }
+    end
   end
+
   def show
+    @user = User.find_by(id: params[:id])
+    render :json => @user, include: [:projects => {:include => [:section_titles, :graphs, :data => {:only => [:name, :type, :url, :description, :content]}]}]
   end
+=begin
   def new
-  end
-  def create
+    @user = User.new
   end
   def edit
   end
+=end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: {
+        status: 500,
+        message: 'Creating user failed.'
+      }
+    end
+  end
+
   def update
+    @user = User.find_by(id: params[:id])
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: {
+        status: 500,
+        message: 'Editing user failed.'
+      }
+    end
   end
   def destroy
+    @user = User.find_by(id: params[:id])
+    if @user.destroy
+      render json: {
+        status: 200,
+        message: 'Deleting user succeeded.'
+      }
+    else
+      render json: {
+        status: 500,
+        message: 'Deleting user failed.'
+      }
+    end
   end
   private
   def user_params
