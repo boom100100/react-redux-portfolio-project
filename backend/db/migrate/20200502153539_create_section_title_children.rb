af3a1b9e01690bd2aa833d9fe4a7f496b43efb25
boom100100 <bsd231@nyu.edu>
2020-05-02 12:01:54 -0400

    Added sti migration.

---
+++ backend/db/migrate/20200502153539_create_section_title_children.rb
@@ -0,0 +1,18 @@
+class CreateSectionTitleChildren < ActiveRecord::Migration[6.0]
+  def change
+    create_table :section_title_children do |t|
+      t.string :name
+      t.string :url
+      t.string :description
+      t.string :content, :null => true
+
+      t.integer :obj_order
+
+      t.string :type
+
+      t.belongs_to :section_title
+
+      t.timestamps
+    end
+  end
+end
