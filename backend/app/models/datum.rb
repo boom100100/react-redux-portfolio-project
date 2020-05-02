962e61ea7f9ef474bea18ebc8d899bab740fc5db
boom100100 <bsd231@nyu.edu>
2020-05-02 11:59:45 -0400

    Added validations for ordering section title values.

--- backend/app/models/datum.rb
+++ backend/app/models/datum.rb
@@ -2,6 +2,8 @@ class Datum < SectionTitleChild
   validates :name, presence: true
   validates :url, presence: true
   validates :presence, presence: true
+  validates :obj_order, presence: true
   validates :content, presence: true
+  
   validates :type, presence: true
 end
--- backend/app/models/graph.rb
+++ backend/app/models/graph.rb
@@ -2,6 +2,8 @@ class Graph < SectionTitleChild
   validates :name, presence: true
   validates :url, presence: true
   validates :presence, presence: true
+  validates :obj_order, presence: true
   validates :content, presence: false
+  
   validates :type, presence: true
 end
