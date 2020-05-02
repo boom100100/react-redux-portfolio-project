712065ebe6f29e7d3ffc3db74264263299335cf2
boom100100 <bsd231@nyu.edu>
2020-05-02 18:27:14 -0400

    Added login container, gave redux support.

--- frontend/src/containers/LoginContainer.js
+++ frontend/src/containers/LoginContainer.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { login } from  '../actions/login';
+
 import LoginComponent from '../components/LoginComponent'
 
 class LoginContainer extends Component {
-  // TODO: move to redux thunk - signin info needed on each page
-  state = {
-    email: "",
-    password: "",
-    password_confirmation: ""
+  constructor(props){
+    super(props);
+
+    // TODO: move to redux thunk - signin info needed on each page
+    this.state = {
+      email: "",
+      password: ""
+    }
   }
+
+
 
   handleOnSubmit = (event) => {
     event.preventDefault();
+    console.log(this.state);
+    this.props.login(this.state);
+    this.setState({email: "", password: ""});
     //add dispatch
   }
 
@@ -19,16 +30,40 @@ class LoginContainer extends Component {
       [event.target.name]: event.target.value
     });
     //setState is asynchronous
-    console.log(this.state)
+    //console.log(this.state)
   }
 
   render(){
     return (
       <div>
         <LoginComponent handleOnChange={this.handleOnChange} handleOnSubmit={this.handleOnSubmit} myState={this.state} />
+        {// TODO: remove button
+
+        }<button onClick={this.handleOnClick}>Click</button>
+
       </div>
     )
   }
 }
 
-export default LoginContainer;
+const mapStateToProps = state => {
+  return {
+    email: state.email,
+    password: state.password
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    increaseCount: () => dispatch({ type: 'INCREASE_COUNT' }),
+    login: (state) => dispatch(login(state)),
+
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoginContainer);
+
+// export default LoginContainer;
