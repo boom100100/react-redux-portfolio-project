954caf38097b08cbfa5f50f8acb2f2394db1f187
boom100100 <bsd231@nyu.edu>
2020-05-02 12:02:46 -0400

    Modified seeds for sti section_title_children migration.

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -17,14 +17,14 @@ project_a = Project.create(name: 'a', abstract: 'a', user: user_a)
 title_a = SectionTitle.create(name: 'a', project: project_a)
 title_b = SectionTitle.create(name: 'b', project: project_a)
 
-prelim_data_a = PreliminaryDatum.create(name: 'a', url: 'https://google.com', description: 'a', content: 'a', project: project_a, section_title: title_a)
-prelim_data_b = PreliminaryDatum.create(name: 'b', url: 'https://google.com', description: 'b', content: 'b', project: project_a)
+prelim_data_a = PreliminaryDatum.create(name: 'a', url: 'https://google.com', description: 'a', content: 'a', section_title: title_a)
+prelim_data_b = PreliminaryDatum.create(name: 'b', url: 'https://google.com', description: 'b', content: 'b', section_title: title_b)
 
-research_data_a = ResearchDatum.create(name: 'a', url: 'https://google.com', description: 'a', content: 'a', project: project_a, section_title: title_a)
-research_data_b = ResearchDatum.create(name: 'b', url: 'https://google.com', description: 'b', content: 'b', project: project_a)
+research_data_a = ResearchDatum.create(name: 'a', url: 'https://google.com', description: 'a', content: 'a', section_title: title_a)
+research_data_b = ResearchDatum.create(name: 'b', url: 'https://google.com', description: 'b', content: 'b', section_title: title_b)
 
-random_data_a = RandomDatum.create(name: 'a', url: 'https://google.com', description: 'a', content: 'a', project: project_a, section_title: title_a)
-random_data_b = RandomDatum.create(name: 'b', url: 'https://google.com', description: 'b', content: 'b', project: project_a)
+random_data_a = RandomDatum.create(name: 'a', url: 'https://google.com', description: 'a', content: 'a', section_title: title_a)
+random_data_b = RandomDatum.create(name: 'b', url: 'https://google.com', description: 'b', content: 'b', section_title: title_b)
 
-graph_a = Graph.create(name: 'a', url: 'https://google.com', description: 'a', project: project_a, section_title: title_a)
-graph_b = Graph.create(name: 'b', url: 'https://google.com', description: 'b', project: project_a)
+graph_a = Graph.create(name: 'a', url: 'https://google.com', description: 'a', section_title: title_a)
+graph_b = Graph.create(name: 'b', url: 'https://google.com', description: 'b', section_title: title_b)
