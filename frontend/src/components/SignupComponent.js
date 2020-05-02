53200867086e3d4a2696946214a981fb0c981762
boom100100 <bsd231@nyu.edu>
2020-05-02 18:23:48 -0400

    Added login, signup components.

--- frontend/src/components/SignupComponent.js
+++ frontend/src/components/SignupComponent.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-const LoginComponent = (props) => {
+const SignupComponent = (props) => {
 
   return (
     <form onSubmit={props.handleOnSubmit} /*action="http://localhost:3001/sessions" method="POST"*/>
       <input type="text" name="email" onChange={props.handleOnChange} value={props.myState.email} />
       <input type="password" name="password" onChange={props.handleOnChange} value={props.myState.password} />
+      <input type="password" name="password_confirmation" onChange={props.handleOnChange} value={props.passwordConfirmation} />
       <input type="submit" value="Log In" />
     </form>
   )
 }
 
-export default LoginComponent;
+export default SignupComponent;
