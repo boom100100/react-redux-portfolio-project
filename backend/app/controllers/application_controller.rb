d58b85d0d377dc34fbcc07504ac8d5b1edb1c299
boom100100 <bsd231@nyu.edu>
2020-05-04 18:27:15 -0400

    Added cookie support.

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
   include ActionController::RequestForgeryProtection
 
-  protect_from_forgery with: :exception
-  before_action :set_csrf_cookie
+  #protect_from_forgery #with: :exception
+  #before_action :set_csrf_cookie
 
   private
 
--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,16 +1,24 @@
 class SessionsController < ApplicationController
+  #skip_before_action :set_csrf_cookie, only: [:create]
+  def new
+    render json: {token: cookies["CSRF-TOKEN"]}
+  end
+
   def create
     user = User.find_by(email: params[:email])
 
     user.try(:authenticate, params[:password])
 
-    return render json: {response: "Login failed."} unless user
+    return render json: {message: "Login failed."}, status: 400 unless user
 
     session[:user_id] = user.id
-    render :json => session
+    # TODO: how to securely send and store cookie?
+    render json: {data:{user: user.email}}
 
   end
 
   def destroy
+    reset_session
+    render json: {message: 'Session was ended.'}
   end
 end
