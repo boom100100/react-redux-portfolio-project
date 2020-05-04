4f4c63f7d3f99ca52f2bb57cf28c0eee0512a376
boom100100 <bsd231@nyu.edu>
2020-05-04 18:28:57 -0400

    Added cookie support.

--- frontend/src/actions/SessionActions.js
+++ frontend/src/actions/SessionActions.js
@@ -1,11 +1,33 @@
 import { sessionService } from 'redux-react-session';
 import * as sessionApi from '../api/SessionApi';
 
-export const login = (user, history) => {
+export const newSession = (setStateFunction, state) => {
   return () => {
-    console.log(user);
-    console.log(history);
-    return sessionApi.login(user);
+    return sessionApi.newSession().then(response => response.json());
+  }
+
+};
+
+
+export const login = (state, token, history) => {
+  return () => {
+    return sessionApi.login(state, token).then(response => response.json()).then(json => {
+      // TODO: is this secure way to store token?
+
+      //json includes token from server
+      const { cred_token } = json;
+
+      sessionService.saveSession({ cred_token })
+      .then(() => {
+
+        sessionService.saveUser(json.data)
+        .then(() => {
+          localStorage.setItem("loggedIn", true);
+          localStorage.setItem("user", JSON.stringify(json.data.user));
+          history.push('/');
+        }).catch(err => console.error(err));
+      }).catch(err => console.error(err));
+    });
   };
 };
 
