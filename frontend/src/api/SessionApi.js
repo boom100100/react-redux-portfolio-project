b04ec79363791a534a2c841c4fe29eaaf5030c17
boom100100 <bsd231@nyu.edu>
2020-05-04 18:29:28 -0400

    Added cookie support.

--- frontend/src/api/SessionApi.js
+++ frontend/src/api/SessionApi.js
@@ -1,58 +1,46 @@
-import { sessionService } from 'redux-react-session';
 
-// Simulates server calls
+export const newSession = () => {
+  let url = 'http://localhost:3001/sessions/new';
+  let configData = {method: "GET", headers: {
+      "Host": 'http://localhost:3000',
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+  };
 
-export const login = (user) => {
+  return fetch(url);
+}
+
+export const login = (state, token) => {
   let url = 'http://localhost:3001/sessions';
   let configData = {
     method: "POST",
+    //"Credentials": "include",
     headers: {
       "Host": 'http://localhost:3000',
       "Content-Type": "application/json",
       "Accept": "application/json"
     },
-    body: JSON.stringify( user )
+    body: JSON.stringify( state.user )
   };
 
-  return fetch(url, configData)
-      .then(response => response.json())
-      .then(json => {
-        console.log('User is ' );
-        console.log(user);
-        console.log('Json is ' );
-        console.log(json);
+  return fetch(url, configData);
 
-        //return json;
-
-        const { token } = json;
-        sessionService.saveSession({ token })
-        .then(() => {
-          sessionService.saveUser(json.data)
-          .then(() => {
-            window.history.push('/');
-            console.log("Log in function called, completed.")
-            console.log(json)
-
-            console.log('Login completed.');
-          }).catch(err => console.error(err));
-        }).catch(err => console.error(err));
-      })
-      .catch(error => console.error(error));
-
-
-
-
-    /*const response = {
-    token: '1a2b3c4d',
-    data: {
-      email: user.email,
-      firstName: 'test',
-      lastName: 'test'
-    }
-  };*/
-  //return new Promise(resolve => setTimeout(resolve(response), 1000));
-};
+}
 
 export const logout = () => {
-  return new Promise(resolve => setTimeout(resolve, 1000));
+  let url = 'http://localhost:3001/sessions';
+  let configData = {
+    method: "DELETE",
+    headers: {
+      "Host": 'http://localhost:3000',
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+      'Credentials': 'true'
+    },
+    // TODO: MODIFY 'user'
+    body: JSON.stringify( 'user' )
+  };
+
+  return fetch(url, configData);
 }
