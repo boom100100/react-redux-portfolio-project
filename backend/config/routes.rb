b0d941523838f1397df26f9c0b877d05df561a18
boom100100 <bsd231@nyu.edu>
2020-05-05 10:41:16 -0400

    Removed unnecessary route.

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :sessions, only: [:new,:create, :destroy]
+  resources :sessions, only: [:create, :destroy]
   resources :users, except: [:new, :edit]
 end
