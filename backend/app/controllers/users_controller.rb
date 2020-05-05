70f632d861d9baf8cbc44e756bc865a4f64c179b
boom100100 <bsd231@nyu.edu>
2020-05-05 16:03:00 -0400

    Fixed create flow.

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -25,14 +25,14 @@ class UsersController < ApplicationController
   end
 =end
   def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: @user
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      render json: {data:{user: user.email}}
     else
       render json: {
-        status: 500,
         message: 'Creating user failed.'
-      }
+      }, status: 500
     end
   end
 
@@ -63,6 +63,6 @@ class UsersController < ApplicationController
   end
   private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.permit(:email, :password, :password_confirmation)
   end
 end
