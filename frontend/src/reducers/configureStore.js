12abd559603c1191a96b43d796b6d57af482b53f
boom100100 <bsd231@nyu.edu>
2020-05-08 16:12:58 -0400

    Persisted state.

---
+++ frontend/src/reducers/configureStore.js
@@ -0,0 +1,20 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { sessionService } from 'redux-react-session';
+
+
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+import rootReducer from './rootReducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export let store = createStore(persistedReducer, applyMiddleware(thunk));
+sessionService.initSessionService(store);
+export let persistor = persistStore(store);
