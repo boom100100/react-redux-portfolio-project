21599bbe06202326e4b60ab32bdfacd4351136c7
boom100100 <bsd231@nyu.edu>
2020-05-08 16:19:01 -0400

    Added project reducer.

--- frontend/src/reducers/projectReducer.js
+++ frontend/src/reducers/projectReducer.js
@@ -2,8 +2,10 @@ export default function projectReducer(state = [], action){
   switch(action.type){
     case 'REPLACE_PROJECTS':
       return action.projects;
+
     case 'NEW_PROJECT':
       return [...state, action.projects];
+      
     default:
       return state;
   }
