867ba8a451f4bd8ecc17b77a3fdf978f60e36515
boom100100 <bsd231@nyu.edu>
2020-05-11 10:10:48 -0400

    Edited config for store.

--- frontend/src/reducers/configureStore.js
+++ frontend/src/reducers/configureStore.js
@@ -1,8 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+
 import { sessionService } from 'redux-react-session';
-
-
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
@@ -11,10 +11,11 @@ import rootReducer from './rootReducer';
 const persistConfig = {
   key: 'root',
   storage,
+  stateReconciler: autoMergeLevel2,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export let store = createStore(persistedReducer, applyMiddleware(thunk));
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
 sessionService.initSessionService(store);
-export let persistor = persistStore(store);
+export const persistor = persistStore(store);
