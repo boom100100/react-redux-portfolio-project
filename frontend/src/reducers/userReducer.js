55c3c9b6e53a2ca8606ea0ec06a033db099c69ad
boom100100 <bsd231@nyu.edu>
2020-05-11 11:21:01 -0400

    Commented out logs.

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -2,30 +2,30 @@ function userReducer(state = {email: ''}, action) {
   //debugger;
   switch (action.type) {
     case "ADD_USER":
-      console.log(state);
+      // console.log(state);
       let newState = {email: action.user.email};
-      console.log("Doing ADD_USER.");
-      console.log(newState);
+      // console.log("Doing ADD_USER.");
+      // console.log(newState);
       // newState = state.set('projects', action.user.projects);
       return newState;
 
 
     case 'RESET_USER':
       let newState2 = {email: ''};
-      console.log("Doing RESET_USER.");
+      // console.log("Doing RESET_USER.");
       return newState2;
 
      case 'EDIT_USER':
-      console.log("Doing EDIT_USER.");
-      console.log(action.user.email);
+      // console.log("Doing EDIT_USER.");
+      // console.log(action.user.email);
       return {email: action.user.email};
 
 
 
     default:
       //always returns default
-      console.log("Doing default.");
-      console.log(state);
+      // console.log("Doing default.");
+      // console.log(state);
       return state;
   }
 }
