779cd358bdb7f9082762f4bdc4d89ed1797c7806
boom100100 <bsd231@nyu.edu>
2020-05-11 10:00:51 -0400

    DRY'd up code.

--- frontend/src/actions/SessionActions.js
+++ frontend/src/actions/SessionActions.js
@@ -1,56 +1,55 @@
 import { sessionService } from 'redux-react-session';
 import * as sessionApi from '../api/SessionApi';
-
+import { persistor } from '../reducers/configureStore'
 /*
 all calls go to api
-saves to store
+saves to session state
+saves to user state
 saves to localStorage as well, but nothing dangerous goes there
 */
 
 export const login = (state, history, addUser) => {
   return () => {
-    return sessionApi.login(state).then(response => response.json()).then(json => {
-
-      // console.log(json);
-
-      // const cred_token = json;
-
-      // console.log(sessionService);
-      // console.log(sessionService.saveSession);
-      // console.log(sessionService.saveUser);
-      sessionService.saveSession({ json })
-      .then(() => {
-
-        sessionService.saveUser(json)
-        .then(() => {
-          if (json.email) {
-            console.log(state);
-            console.log(json);
-
-            addUser(json);
-            //dispatch(addUser(json));
-            localStorage.setItem("loggedIn", true);
-            history.push('/');
-            //history.go(0);
-          }
-        }).catch(err => console.error(err));
-      }).catch(err => console.error(err));
-    });
+    return sessionApi.login(state)
+      .then(response => response.json())
+      .then(json => {
+        doSessionSave(json, history, addUser);
+      });
   };
 };
 
-export const logout = (history) => {
+export const logout = (history, resetUser) => {
   return () => {
     return sessionApi.logout().then(() => {
-      sessionService.deleteSession();
-      sessionService.deleteUser();
-      localStorage.setItem("loggedIn", false);
-      //localStorage.clear("user");
-
-      history.push('/login');
-      history.go(0);
+      doSessionDelete(history, resetUser);
     }).catch(err => {
       throw (err);
     });
   };
 };
+
+export const doSessionSave = (json, history, addUser) => {
+  sessionService.saveSession({ id: json.id })
+    .then(() => {
+      sessionService.saveUser(json)
+      .then(() => {
+        if (json.email) {
+          // TODO: userReducer(json, {type: 'LOGIN_USER', user: json});
+          addUser(json);
+          localStorage.setItem("loggedIn", true);
+          history.push('/');
+          history.go(0);
+        }
+      }).catch(err => console.error(err));
+    }).catch(err => console.error(err));
+}
+
+export const doSessionDelete = (history, resetUser) => {
+  sessionService.deleteSession();
+  sessionService.deleteUser();
+  localStorage.setItem("loggedIn", false);
+  resetUser();
+  persistor.flush();
+  history.push('/login');
+  history.go(0);
+}
--- frontend/src/actions/UserActions.js
+++ frontend/src/actions/UserActions.js
@@ -1,60 +1,72 @@
 import * as userApi from '../api/UserApi';
 import { sessionService } from 'redux-react-session';
+import { persistor } from '../reducers/configureStore';
+import { logout, doSessionSave } from '../actions/SessionActions';
 
 export const addUser = (user) => {
   //console.log(user);
   // debugger;
-  const action = {type: 'ADD_USER', user: {email: user.email, projects: user.projects} };
+  const action = {type: 'ADD_USER', user };
   console.log("Action is");
   console.log((action));
   return action;
 }
 
 export const resetUser = () => {
-  const action = {type: 'RESET_USER', user: {email: '', projects: []} };
-  console.log((action));
+  const action = {type: 'RESET_USER' };
+  console.log(action);
   return action;
 }
 
-export const createUser = (state, history) => {
+export const createUser = (state, history, addUser) => {
 
   return () => {
     return userApi.createUser(state)
       .then(response => response.json())
       .then(json => {
-
-        sessionService.saveSession({ json })
-          .then(() => {
-
-            sessionService.saveUser(json)
-            .then(() => {
-              if (json) {
-                // TODO: userReducer(json, {type: 'LOGIN_USER', user: json});
-                localStorage.setItem("loggedIn", true);
-                localStorage.setItem("user", JSON.stringify(json.user));
-                history.push('/');
-                history.go(0);
-              }
-            }).catch(err => console.error(err));
-          }).catch(err => console.error(err));
-
-
-      }).catch(error => console.log(error));
+        doSessionSave(json, history, addUser);
+        }).catch(error => console.log(error));
   }
 }
 
 
-export const editUser = (state) => {
-  return {
-    type: 'EDIT_USER',
-    state: state
-  };
-};
 
 
-export const deleteUser = (state) => {
-  return {
-    type: 'DELETE_USER',
-    state: state
-  };
-};
+export const updateUser = (state, history, updateUserState) => {
+
+  return () => {
+    return userApi.updateUser(state)
+      .then(response => response.json())
+      .then(json => {
+        if (json.email) {
+          console.log(json);
+          updateUserState(json);
+          history.push('/profile');
+          history.go(0);
+        }
+      })
+      .catch(err => console.error(err));
+  }
+}
+export const updateUserState = (user) => {
+  const action = {type: 'EDIT_USER', user };
+  return action;
+}
+
+
+export const deleteUser = (state, history, logout, resetUser) => {
+  return () => {
+    return userApi.deleteUser(state)
+      .then(response => {
+
+        if (response.ok) {
+          console.log(response);
+          logout(history, resetUser);
+          // TODO: fix history
+          history.push('/profile');
+          history.go(0);
+        }
+      })
+      .catch(err => console.error(err));
+  }
+}
