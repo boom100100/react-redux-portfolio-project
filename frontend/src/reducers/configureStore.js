f2c36b34a55ea1af125b8be37f8ae010050067a4
boom100100 <bsd231@nyu.edu>
2020-05-13 11:32:32 -0400

    Refined config store settings.

--- frontend/src/reducers/configureStore.js
+++ frontend/src/reducers/configureStore.js
@@ -1,9 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
 
 import { sessionService } from 'redux-react-session';
-import { persistStore, persistReducer } from 'redux-persist'
+import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 import rootReducer from './rootReducer';
@@ -11,7 +10,6 @@ import rootReducer from './rootReducer';
 const persistConfig = {
   key: 'root',
   storage,
-  stateReconciler: autoMergeLevel2,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
