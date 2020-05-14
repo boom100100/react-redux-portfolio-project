23cf79a114cc1b75ae42812f3c005d74c91e15e0
boom100100 <bsd231@nyu.edu>
2020-05-13 21:41:23 -0400

    Updated #getData to include urlSearchTerm.

--- frontend/src/actions/DataResearchActions.js
+++ frontend/src/actions/DataResearchActions.js
@@ -12,10 +12,10 @@ export const getPreliminaryData = (updatefetchData, urlSearchTerm) => {
   return getData(dataApi.getPreliminaryData, updatefetchData, urlSearchTerm);
 }
 
-export const getData = (doFetch, updatefetchData) => {
+export const getData = (doFetch, updatefetchData, urlSearchTerm) => {
   return () => {
     console.log('random action');
-    return doFetch()
+    return doFetch(urlSearchTerm)
       .then(response => response.json())
       .then(json => {
         console.log(json);
