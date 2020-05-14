2e7fd765341dbad7e5cee2a30fa7379c266ca1cb
boom100100 <bsd231@nyu.edu>
2020-05-13 21:39:52 -0400

    Modified seeds to include order.

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -14,17 +14,17 @@ user_e = User.create(email: 'e@e.e', password: 'eeeeee', password_confirmation: 
 
 project_a = Project.create(name: 'a', abstract: 'a', user: user_a)
 
-title_a = SectionTitle.create(name: 'a', project: project_a)
-title_b = SectionTitle.create(name: 'b', project: project_a)
+title_a = SectionTitle.create(name: 'a', project: project_a, section_order: 0)
+title_b = SectionTitle.create(name: 'b', project: project_a, section_order: 1)
 
-prelim_data_a = PreliminaryDatum.create(name: 'a', url: 'https://google.com', description: 'a', content: 'a', section_title: title_a)
-prelim_data_b = PreliminaryDatum.create(name: 'b', url: 'https://google.com', description: 'b', content: 'b', section_title: title_b)
+prelim_data_a = PreliminaryDatum.create(name: 'a', url: 'https://google.com', description: 'a', content: 'a', section_title: title_a, child_order: 0)
+prelim_data_b = PreliminaryDatum.create(name: 'b', url: 'https://google.com', description: 'b', content: 'b', section_title: title_b, child_order: 1)
 
-research_data_a = ResearchDatum.create(name: 'a', url: 'https://google.com', description: 'a', content: 'a', section_title: title_a)
-research_data_b = ResearchDatum.create(name: 'b', url: 'https://google.com', description: 'b', content: 'b', section_title: title_b)
+research_data_a = ResearchDatum.create(name: 'a', url: 'https://google.com', description: 'a', content: 'a', section_title: title_a, child_order: 2)
+research_data_b = ResearchDatum.create(name: 'b', url: 'https://google.com', description: 'b', content: 'b', section_title: title_b, child_order: 3)
 
-random_data_a = RandomDatum.create(name: 'a', url: 'https://google.com', description: 'a', content: 'a', section_title: title_a)
-random_data_b = RandomDatum.create(name: 'b', url: 'https://google.com', description: 'b', content: 'b', section_title: title_b)
+random_data_a = RandomDatum.create(name: 'a', url: 'https://google.com', description: 'a', content: 'a', section_title: title_a, child_order: 4)
+random_data_b = RandomDatum.create(name: 'b', url: 'https://google.com', description: 'b', content: 'b', section_title: title_b, child_order: 5)
 
-graph_a = Graph.create(name: 'a', url: 'https://google.com', description: 'a', section_title: title_a)
-graph_b = Graph.create(name: 'b', url: 'https://google.com', description: 'b', section_title: title_b)
+graph_a = Graph.create(name: 'a', url: 'https://google.com', description: 'a', section_title: title_a, child_order: 6)
+graph_b = Graph.create(name: 'b', url: 'https://google.com', description: 'b', section_title: title_b, child_order: 7)
