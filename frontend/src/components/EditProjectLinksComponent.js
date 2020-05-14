ebfdffaae703f3a95610b815b0ac6166b9db66b2
boom100100 <bsd231@nyu.edu>
2020-05-13 21:54:06 -0400

    Added links to editproject.

--- frontend/src/components/EditProjectLinksComponent.js
+++ frontend/src/components/EditProjectLinksComponent.js
@@ -3,21 +3,17 @@ import React from 'react';
 const EditProjectLinksComponent = (props) => {
   return (
     <div>
-      <div id='links'>
-
+      <div id='primary-links'>
           <button onClick={props.functions.addSectionTitle}>Add Section Title</button>
           <button onClick={props.functions.showAddDataOptions}>Add Research</button>
           <button onClick={props.functions.addGraph}>Add Graph</button>
-
       </div>
 
       <div id='secondary-links' >
-
-          <button onClick={props.functions.addSectionTitle}>Add Preliminary Data</button>
-          <button onClick={props.functions.showAddDataOptions}>Add Research Data</button>
-          <button onClick={props.functions.addGraph}>Add Random Data</button>
+          <button onClick={props.functions.addPreliminaryData}>Add Preliminary Data</button>
+          <button onClick={props.functions.addResearchData}>Add Research Data</button>
+          <button onClick={props.functions.addRandomData}>Add Random Data</button>
           <button onClick={props.functions.showPrimaryOptions}>Back</button>
-
       </div>
     </div>
 
