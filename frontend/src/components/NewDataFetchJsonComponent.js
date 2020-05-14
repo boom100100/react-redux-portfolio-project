c3561108dabb4272a8913ff9b9a347119301344c
boom100100 <bsd231@nyu.edu>
2020-05-14 11:06:14 -0400

    Removed holdText.

--- frontend/src/components/NewDataFetchJsonComponent.js
+++ frontend/src/components/NewDataFetchJsonComponent.js
@@ -4,7 +4,7 @@ import React from 'react';
 const NewDataFetchJsonComponent = (props) => {
   // const titles = props.state.project.section_titles.map(title => <SectionTitleComponent title={title} />)
   return (
-    <div id={props.inputFields.names.divIdFetch}>{(props.inputFields.holdText || undefined)}</div>
+    <div id={props.inputFields.names.divIdFetch}></div>
   )
 }
 
