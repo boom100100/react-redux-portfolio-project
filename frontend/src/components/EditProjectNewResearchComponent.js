69ed742290aa2dc505ef7dc51891395aaee16d55
boom100100 <bsd231@nyu.edu>
2020-05-13 21:54:43 -0400

    Added research data containers.

--- frontend/src/components/EditProjectNewResearchComponent.js
+++ frontend/src/components/EditProjectNewResearchComponent.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import NewRandomDataContainer from '../containers/NewRandomDataContainer';
+import NewPreliminaryDataContainer from '../containers/NewPreliminaryDataContainer';
+import NewResearchDataContainer from '../containers/NewResearchDataContainer';
 
 const EditProjectNewResearchComponent = (props) => {
   return (
-    <>
-      <div id='research-data-creator'>
-        Research holding.
-      </div>
-    </>
+    <div id='add-new-research'>
+      <NewRandomDataContainer />
+      <NewPreliminaryDataContainer />
+      <NewResearchDataContainer />
+    </div>
   )
 }
 
