d22911b75eece34e0aaf277772ee93390949474b
boom100100 <bsd231@nyu.edu>
2020-05-18 17:23:02 -0400

    Generalized GenericSearchComponent.

--- frontend/src/containers/EditProjectNewSectionTitleContainer.js
+++ frontend/src/containers/EditProjectNewSectionTitleContainer.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import GenericSearchComponent from  '../components/GenericSearchComponent';
+import NewDataFetchJsonComponent from  '../components/NewDataFetchJsonComponent';
+import NewDataInputFieldsComponent from  '../components/NewDataInputFieldsComponent';
 
 class EditProjectNewSectionTitleContainer extends Component {
+  getNewData = () => {}
+
+  divIdFetch = () => 'section-title-fetch-json';
+  divIdInput = () => 'section-title-input-fields';
+
+  state = {
+    fetchData: [],
+    inputFields: {
+      names: {
+        divIdFetch: this.divIdFetch(),
+        divIdInput: this.divIdInput()
+      },
+      searchTerm: "",
+      holdText: "Enter a topic word or phrase and click Search.",
+      name: "",
+      url: "",
+      description:  "",
+      content: ""
+    }
+  }
+
   render(){
     return (
       <div id='add-new-section-title'>
-        SectionTitle holder.
+        <GenericSearchComponent text={'Topic: '}inputFields={this.state.inputFields} searchTerm={this.state.searchTerm} click={this.getNewData} onChange={this.onChange} />
+
+        <NewDataFetchJsonComponent fetchData={this.state.fetchData} inputFields={this.state.inputFields} />
+        <NewDataInputFieldsComponent inputFields={this.state.inputFields} click={this.saveToProject} onChange={this.onChange} />
       </div>
     )
   }
