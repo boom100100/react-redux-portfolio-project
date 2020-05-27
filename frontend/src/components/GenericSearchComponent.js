b19fdd7f739405eda82f00bb1330a33f818e26c6
boom100100 <bsd231@nyu.edu>
2020-05-27 18:39:17 -0400

    Created custom tag.

--- frontend/src/components/GenericSearchComponent.js
+++ frontend/src/components/GenericSearchComponent.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const GenericSearchComponent = (props) => {
+  const CustomTag = `${props.type}`;
   return (
     <div>
-      {props.text}<input name='searchTerm' value={props.inputFields.searchTerm} onChange={e => props.onChange(e)} />
+      {props.text}<CustomTag name='searchTerm' value={props.inputFields.searchTerm} onChange={e => props.onChange(e)} />
+
       <button onClick={props.click}>Search</button>
       <p>{(props.inputFields.holdText || undefined)}</p>
     </div>
