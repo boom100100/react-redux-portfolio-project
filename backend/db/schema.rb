18a347f75b1ec38ac53afd5a86573fc13fa67294
boom100100 <bsd231@nyu.edu>
2020-05-27 18:56:04 -0400

    Saving db schema.

---
+++ backend/db/schema.rb
@@ -0,0 +1,53 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_05_02_153539) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "abstract"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
+  create_table "section_title_children", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.string "description"
+    t.string "content"
+    t.integer "child_order"
+    t.string "type"
+    t.integer "section_title_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["section_title_id"], name: "index_section_title_children_on_section_title_id"
+  end
+
+  create_table "section_titles", force: :cascade do |t|
+    t.string "name", default: "unnamed-id"
+    t.integer "section_order"
+    t.integer "project_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_section_titles_on_project_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
