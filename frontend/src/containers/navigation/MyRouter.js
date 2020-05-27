026adaad4b556f77ba3feb340bd512fe55f09d70
boom100100 <bsd231@nyu.edu>
2020-05-27 18:53:51 -0400

    Commented out routes.

--- frontend/src/containers/navigation/MyRouter.js
+++ frontend/src/containers/navigation/MyRouter.js
@@ -30,7 +30,7 @@ const MyRouter = (props) => {
       <Route path="/projects/:project_id/read" component={ProjectContainer} />
       <Route path="/projects/:project_id/edit" component={EditProjectContainer} />
       <Route path="/projects/:project_id/delete" component={ProjectContainer} />
-      <Route path="/projects/:project_id/research-data" render={() => <div>Fake ID and research data</div>} />
+      {/*<Route path="/projects/:project_id/research-data" render={() => <div>Fake ID and research data</div>} />
       <Route path="/projects/:project_id/research-data/:data_id" render={() => <div>Fake ID and research data</div>} />
       <Route path="/projects/:project_id/preliminary-data" render={() => <div>Fake ID and preliminary data</div>} />
       <Route path="/projects/:project_id/preliminary-data/:data_id" render={() => <div>Fake ID and preliminary data</div>} />
@@ -39,7 +39,7 @@ const MyRouter = (props) => {
       <Route path="/projects/:project_id/graphs" render={() => <div>Fake ID and graph maker</div>} />
       <Route path="/projects/:project_id/graphs/:graph_id" render={() => <div>Fake ID and graph maker</div>} />
       <Route path="/projects/:project_id/section-titles" render={() => <div>Fake ID and section title writer</div>} />
-      <Route path="/projects/:project_id/section-titles/:title_id" render={() => <div>Fake ID and section title writer</div>} />
+      <Route path="/projects/:project_id/section-titles/:title_id" render={() => <div>Fake ID and section title writer</div>} />*/}
       <Route path="/random-data-finder" render={() => <div>Fake ID and section title writer</div>} />
       {/*Routes:
 
