508af6e242224823deafaefa7d077c8e14a1bd35
boom100100 <bsd231@nyu.edu>
2020-05-28 10:25:47 -0400

    Added save section title to state, reordering #levelUp and sorting #sortSection to projects reducer.

--- frontend/src/reducers/projectsReducer.js
+++ frontend/src/reducers/projectsReducer.js
@@ -22,11 +22,42 @@ function projectsReducer(state = [], action){
       let newState = [...state];
       const projectId = action.sectionTitle.project_id - 1;
       newState[projectId].section_titles.push(action.sectionTitle);
+      console.log('action', action);
+      let section_titles = newState[projectId].section_titles;
+      console.log('section_titles', section_titles);
+
+      //add 1 to equal and following sections
+      section_titles = levelUp(section_titles, action.sectionTitle);
+      console.log('section_titles before sort', section_titles);
+
+      //sort data
+      newState[projectId].section_titles = section_titles.sort((a, b) => sortSection(a, b));
+      console.log('section_titles after sort', section_titles);
       return newState;
 
     default:
       return state;
   }
+}
+
+const levelUp = (section_titles, sectionTitle) => {
+  console.log(sectionTitle.section_order);
+  return section_titles.map((e) => {
+    if (e.section_order >= sectionTitle.section_order && sectionTitle !== e)
+      e.section_order = e.section_order + 1
+
+    return e;
+  });
+}
+
+const sortSection = (a, b) => {
+  if (a.section_order < b.section_order) {
+        return -1;
+    }
+    if (b.section_order < a.section_order) {
+        return 1;
+    }
+    return 0;
 }
 
 export default projectsReducer;
