b5ca5c45963a87b11e19c6c759d217d5dd4c4b57
boom100100 <bsd231@nyu.edu>
2020-05-28 14:31:06 -0400

    Fixed ordering select issue.

--- frontend/src/components/NewDataInputFieldsComponent.js
+++ frontend/src/components/NewDataInputFieldsComponent.js
@@ -2,6 +2,15 @@ import React from 'react';
 import SaveDataButtonComponent from './SaveDataButtonComponent';
 
 const NewDataInputFieldsComponent = (props) => {
+
+  const makeDataOrderOptions = () => {
+    return (
+      <select onChange={e => props.onChangeNumber(e)} onFocus={e => props.onChangeNumber(e)} value={props.inputFields.child_order} name='child_order'>
+        {props.section_titles.map(x => <option key={'id=' + x.id + '-project_id=' + x.project_id + '-name=' + x.name} value={x.id}>{x.name}</option>)}
+        <option value={props.section_titles.length}>(place last)</option>
+      </select>
+    )
+  }
   //order
   //returns inputs for ordering new section titles children
   const letSetOrder = () => {
@@ -19,16 +28,11 @@ const NewDataInputFieldsComponent = (props) => {
       return (
         <span>
           Place in section:
-          <select onChange={e => props.onChangeNumber(e)} onFocus={e => props.onChangeNumber(e)} value={props.inputFields.section_title} name='section_title'>
-            {props.section_titles.map(x => {
-              return (<option key={'section_order=' + x.section_order + '-project_id=' + x.project_id + '-name=' + x.name} value={x.section_order}>{x.name}</option>)
-            })}
+          <select onChange={e => {props.onChangeNumber(e)/*; makeDataOrderOptions()*/}} onFocus={e => props.onChangeNumber(e)} value={props.inputFields.section_title} name='section_title'>
+            {props.section_titles.map(x => <option key={'section_order=' + x.section_order + '-project_id=' + x.project_id + '-name=' + x.name} value={x.section_order}>{x.name}</option>)}
           </select>
-          Place after data:
-          <select onChange={e => props.onChangeNumber(e)} onFocus={e => props.onChangeNumber(e)} value={props.inputFields.child_order} name='child_order'>
-            {props.section_titles.map(x => <option key={'id=' + x.id + '-project_id=' + x.project_id + '-name=' + x.name} value={x.id}>{x.name}</option>)}
-            <option value={props.section_titles.length}>(place last)</option>
-          </select>
+          Place before data:
+          {makeDataOrderOptions()}
         </span>
       );
     }
