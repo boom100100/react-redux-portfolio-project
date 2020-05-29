a92ef5253ab3d628e71dd13e3c6ef0eea90b6108
boom100100 <bsd231@nyu.edu>
2020-05-29 16:57:35 -0400

    Added variables. Commented/removed some logs. Added ADD_DATA. Generalized #levelUp.

--- frontend/src/reducers/projectsReducer.js
+++ frontend/src/reducers/projectsReducer.js
@@ -1,12 +1,18 @@
 function projectsReducer(state = [], action){
+  let newState;
+  let projectId;
+  let section_titles;
+  let section_order;
+  let child_order;
+  let allChildren;
   switch(action.type){
     case 'ADD_PROJECTS':
-      console.log('projects from reducer', action.projects);
-      console.log('new state check', state.concat(action.projects));
+      // console.log('projects from reducer', action.projects);
+      // console.log('new state check', state.concat(action.projects));
       return state.concat(action.projects);
 
     case 'RESET_PROJECTS':
-      console.log('resetting projects in reducer.');
+      // console.log('resetting projects in reducer.');
       return [];
 
     case 'REPLACE_PROJECTS':
@@ -19,42 +25,67 @@ function projectsReducer(state = [], action){
       return [...state];
 
     case 'ADD_SECTION':
-      let newState = [...state];
-      const projectId = action.sectionTitle.project_id - 1;
+      newState = [...state];
+      projectId = action.sectionTitle.project_id - 1;
       newState[projectId].section_titles.push(action.sectionTitle);
-      console.log('action', action);
-      let section_titles = newState[projectId].section_titles;
-      console.log('section_titles', section_titles);
 
-      //add 1 to equal and following sections
-      section_titles = levelUp(section_titles, action.sectionTitle);
-      console.log('section_titles before sort', section_titles);
+      section_titles = newState[projectId].section_titles;
 
-      //sort data
-      newState[projectId].section_titles = section_titles.sort((a, b) => sortSection(a, b));
-      console.log('section_titles after sort', section_titles);
+      //add 1 to section_order equal and following sections
+      section_titles = levelUp(section_titles, action.sectionTitle, 'section_order');
+
+      //sort sections
+      newState[projectId].section_titles = section_titles.sort((a, b) => sortSection(a, b, 'section_order'));
       return newState;
+
+      case 'ADD_DATA':
+        newState = [...state];
+        projectId = action.data.project_id - 1;
+        section_order = action.data.section_order;
+        allChildren = newState[projectId].section_titles[section_order].section_title_children;
+
+
+        child_order = action.data.child_order;
+
+        console.log('ADD_DATA action', action);
+        console.log('newState', newState);
+        //get project
+        console.log('newState[projectId].section_titles[section_order]', newState[projectId].section_titles[section_order]);
+        console.log('newState[projectId].section_titles[section_order].section_title_children', newState[projectId].section_titles[section_order].section_title_children);
+        console.log('action.data', action.data);
+
+        newState[projectId].section_titles[section_order].section_title_children.push(action.data);
+
+        // //add 1 to section_order equal and following sections
+        newState[projectId].section_titles[section_order].section_title_children = levelUp(newState[projectId].section_titles[section_order].section_title_children, action.data, 'child_order');
+        console.log('allChildren before sort', allChildren);
+
+        //sort sections
+        newState[projectId].section_titles[section_order].section_title_children = newState[projectId].section_titles[section_order].section_title_children.sort((a, b) => sortSection(a, b, 'child_order'));
+        console.log('allChildren after sort', allChildren);
+        return newState;
 
     default:
       return state;
   }
 }
 
-const levelUp = (section_titles, sectionTitle) => {
-  console.log(sectionTitle.section_order);
-  return section_titles.map((e) => {
-    if (e.section_order >= sectionTitle.section_order && sectionTitle !== e)
-      e.section_order = e.section_order + 1
+//array, elementArg, key
+const levelUp = (array, elementArg, key) => {
+  console.log(elementArg[key]);
+  return array.map((e) => {
+    if (e[key] >= elementArg[key] && elementArg !== e)
+      e[key] = e[key] + 1
 
     return e;
   });
 }
 
-const sortSection = (a, b) => {
-  if (a.section_order < b.section_order) {
+const sortSection = (a, b, key) => {
+  if (a[key] < b[key]) {
         return -1;
     }
-    if (b.section_order < a.section_order) {
+    if (b[key] < a[key]) {
         return 1;
     }
     return 0;
