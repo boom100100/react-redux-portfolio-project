0e4f0ac5a848e96003ec9f68c117196972046739
boom100100 <bsd231@nyu.edu>
2020-05-29 16:54:39 -0400

    Changed seeds child order.

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -12,19 +12,19 @@ user_c = User.create(email: 'c@c.c', password: 'cccccc', password_confirmation: 
 user_d = User.create(email: 'd@d.d', password: 'dddddd', password_confirmation: 'dddddd')
 user_e = User.create(email: 'e@e.e', password: 'eeeeee', password_confirmation: 'eeeeee')
 
-project_a = Project.create(name: 'a', abstract: 'a', user: user_a)
+project_a = Project.create(name: 'project a', abstract: 'a', user: user_a)
 
-title_a = SectionTitle.create(name: 'a', project: project_a, section_order: 0)
-title_b = SectionTitle.create(name: 'b', project: project_a, section_order: 1)
+title_a = SectionTitle.create(name: 'title 1', project: project_a, section_order: 0)
+title_b = SectionTitle.create(name: 'title 2', project: project_a, section_order: 1)
 
-prelim_data_a = PreliminaryDatum.create(name: 'a', url: 'https://google.com', description: 'a', content: 'a', section_title: title_a, child_order: 0)
-prelim_data_b = PreliminaryDatum.create(name: 'b', url: 'https://google.com', description: 'b', content: 'b', section_title: title_b, child_order: 1)
+prelim_data_a = PreliminaryDatum.create(name: 'prelim 1', url: 'https://google.com', description: 'a', content: 'a', section_title: title_a, child_order: 0)
+prelim_data_b = PreliminaryDatum.create(name: 'prelim 2', url: 'https://google.com', description: 'b', content: 'b', section_title: title_b, child_order: 0)
 
-research_data_a = ResearchDatum.create(name: 'a', url: 'https://google.com', description: 'a', content: 'a', section_title: title_a, child_order: 2)
-research_data_b = ResearchDatum.create(name: 'b', url: 'https://google.com', description: 'b', content: 'b', section_title: title_b, child_order: 3)
+research_data_a = ResearchDatum.create(name: 'research 1', url: 'https://google.com', description: 'a', content: 'a', section_title: title_a, child_order: 1)
+research_data_b = ResearchDatum.create(name: 'research 2', url: 'https://google.com', description: 'b', content: 'b', section_title: title_b, child_order: 1)
 
-random_data_a = RandomDatum.create(name: 'a', url: 'https://google.com', description: 'a', content: 'a', section_title: title_a, child_order: 4)
-random_data_b = RandomDatum.create(name: 'b', url: 'https://google.com', description: 'b', content: 'b', section_title: title_b, child_order: 5)
+random_data_a = RandomDatum.create(name: 'random 1', url: 'https://google.com', description: 'a', content: 'a', section_title: title_a, child_order: 2)
+random_data_b = RandomDatum.create(name: 'random 2', url: 'https://google.com', description: 'b', content: 'b', section_title: title_b, child_order: 2)
 
-graph_a = Graph.create(name: 'a', url: 'https://google.com', description: 'a', section_title: title_a, child_order: 6)
-graph_b = Graph.create(name: 'b', url: 'https://google.com', description: 'b', section_title: title_b, child_order: 7)
+graph_a = Graph.create(name: 'graph 1', url: 'https://google.com', description: 'a', section_title: title_a, child_order: 3)
+graph_b = Graph.create(name: 'graph 2', url: 'https://google.com', description: 'b', section_title: title_b, child_order: 3)
