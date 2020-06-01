8434507c090a71bf8e2ab625842d95a4a847a8ca
boom100100 <bsd231@nyu.edu>
2020-06-01 13:03:45 -0400

    Added backend call.

--- frontend/src/actions/ProjectActions.js
+++ frontend/src/actions/ProjectActions.js
@@ -1,3 +1,5 @@
+import * as projectApi from '../api/ProjectApi';
+
 export const addProjects = (projects) => {
   console.log('projects action', projects);
   return {
@@ -36,10 +38,22 @@ export const addSectionToProject = (sectionTitle) => {
 }
 
 export const addDataToProject = (data) => {
+  console.log('figure out render crash','data', data);
   return {
     type: 'ADD_DATA',
     data: data
-  };
+  }
+}
+
+export const addToBackend = (object, url, method) => {
+  //write thunk action.
+  return () => {
+    return projectApi.addToProject(object, url, method)
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        }).catch(error => console.log(error));
+  }
 }
 
 function junk(){} export default junk;
