c07ec500d8ea6be8beee1f4116f6164c2b9c4cec
boom100100 <bsd231@nyu.edu>
2020-06-03 11:14:10 -0400

    Added :section_order to data.

--- backend/app/controllers/section_title_children_controller.rb
+++ backend/app/controllers/section_title_children_controller.rb
@@ -1,13 +1,13 @@
 class SectionTitleChildrenController < ApplicationController
   def create
-    my_params = {content: params[:content], description: params[:description], name: params[:name], section_title_id: SectionTitle.find_by(section_order: params[:section_order]).id, child_order: params[:section_title_child][:child_order], type: params[:section_title_child][:type], url: params[:section_title_child][:url]}
+    my_params = {content: params[:content], description: params[:description], name: params[:name], section_title_id: SectionTitle.find_by(section_order: params[:section_order]).id, section_order: params[:section_order], child_order: params[:section_title_child][:child_order], type: params[:section_title_child][:type], url: params[:section_title_child][:url]}
     section_title_child = SectionTitleChild.new(my_params)
     if section_title_child.save!
       SectionTitleChild.all.each{|child|
         child.child_order = child.child_order + 1 if (child != section_title_child) && (child.child_order >= section_title_child.child_order) && (child.section_title == section_title_child.section_title)
         child.save
       }
-      render json: {message: 'Created.', section_title_child: section_title_child}
+      render json: {section_title_child: section_title_child, project_id: section_title_child.section_title.project_id, section_order: section_title_child.section_title.section_order }
     end
   end
 
--- backend/app/controllers/section_titles_controller.rb
+++ backend/app/controllers/section_titles_controller.rb
@@ -1,15 +1,23 @@
 class SectionTitlesController < ApplicationController
   def create
     params.delete :section_title
+
     my_params = {name: params[:name], section_order: params[:section_order], project_id: params[:project_id], section_title_children: params[:section_title_children]}
     section_title = SectionTitle.new(my_params)
+    # order = section_title.section_order
 
     if section_title.save!
       #update following section_orders by adding one
       SectionTitle.all.each{|title|
         title.section_order = title.section_order + 1 if (title != section_title) && (title.section_order >= section_title.section_order)
         title.save
+        #children must match parent
+        title.section_title_children.each{|child|
+          child.section_order = child.section_title.section_order
+          child.save
+        }
       }
+      # render json: {message: 'Created.', section_title: section_title, params: params}
       render json: {message: 'Created.', section_title: section_title}
     end
   end
