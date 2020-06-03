6909633b0152fb6b2621832f29df85c1381f1553
boom100100 <bsd231@nyu.edu>
2020-06-03 11:19:26 -0400

    Changed #modifyElements to allow editing.

--- frontend/src/containers/helpers/EditProjectHelpers.js
+++ frontend/src/containers/helpers/EditProjectHelpers.js
@@ -1,50 +1,109 @@
+import React from 'react';
+import EditProjectNewSectionTitleContainer from '../EditProjectNewSectionTitleContainer';
+import EditProjectNewGraphContainer from '../EditProjectNewGraphContainer';
+import NewRandomDataContainer from '../NewRandomDataContainer';
+import NewPreliminaryDataContainer from '../NewPreliminaryDataContainer';
+import NewResearchDataContainer from '../NewResearchDataContainer';
+
 //manages displaying elements
 //and adding divs to show store and fetch data
 
-
+let keyCounter = 0;
 export const modifyElements = (project) => {
   document.getElementById('edit-project-parent').firstElementChild.style.display = 'none';
   let edit = document.getElementById('edit-project-data');
   edit.innerHTML = '';
+  let componentArray = []
+
   for (let section of project.section_titles){
-    //make new div
-
-
-    let parent = document.createElement('div');
-    parent.id = 'edit-project-data-section-' + (section.section_order);
-    let p = document.createElement('p');
-    p.innerText = section.name;
-
-    parent.appendChild(p);
-    for (let data of section.section_title_children){
-      let dataElement = document.createElement('div');
-      dataElement.id = parent.id + '-' + data.name;
-      dataElement.innerText = data.name;
-      parent.appendChild(dataElement);
-    }
-
-    edit.appendChild(parent);
-    //div will contain section name and child divs of data
-    //add to end of edit-project-data, aka edit
-    //include edit button that shows hidden edit fields
+    componentArray.push(section)
+    // console.log('section.section_title_children', section.section_title_children);
+    if (section.section_title_children)
+      for (let data of section.section_title_children){
+        componentArray.push(data)
+        // console.log('pushing data', data);
+      }
   }
+
+  componentArray.sort((a, b) => sorter(a, b));
+
+  return componentArray.map(e => {
+    console.log('e', e);
+    if (!isNaN(e.child_order)) {
+      if (e.type === 'Graph'){
+        // console.log('is graph', e);
+        return (<EditProjectNewGraphContainer key={'graph-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'graph-' + e.section_order + '-' + e.child_order} />);
+      } else if (e.type === 'PreliminaryDatum'){
+        // console.log('is prelim', e);
+        return (<NewPreliminaryDataContainer key={'graph-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'preliminary-data-' + e.section_order + '-' + e.child_order} />);
+      } else if (e.type === 'ResearchDatum'){
+        // console.log('is research', e);
+        return (<NewResearchDataContainer key={'graph-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'research-data-' + e.section_order + '-' + e.child_order} />);
+      } else if (e.type === 'RandomDatum'){
+        // console.log('is random', e);
+        return (<NewRandomDataContainer key={'graph-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'random-data-' + e.section_order + '-' + e.child_order} />);
+      }
+    } else {
+      // console.log('is section title', e);
+      return (<EditProjectNewSectionTitleContainer key={'graph-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'section-title-' + e.section_order} />);
+    }
+  });
 }
 
- const onSave = () => {
-  console.log('clicked save');
-  //dispatch to save project data
-  //in store and on server
-  //on a timer:
-  //project container information,
-  //section_titles information,
-  //section_title_children information
-}
+const sorter = (a, b) => {
+  //SectionTitle: id, section_order
+  //SectionTitleChild: section_title_id, child_order
 
-export const onDelete = () => {
-    console.log('clicked delete');
-    // TODO: where will delete occur?
-    //dispatch to delete project data
-    //in store and on server
+  // if comparing ST and ST
+  // put lower section_order first
+  if (a.id && b.id){
+    if (a.section_order < b.section_order) {
+        return -1;
+    } else if (b.section_order < a.section_order) {
+        return 1;
+    }
+    return 0;
+  }
+
+
+
+
+  // if comparing Child and Child
+  // if section_order === section_order
+  // put lower child_order first
+  else if (a.child_order && b.child_order){
+    if (a.section_order === b.section_order)
+      if (a.child_order < b.child_order) {
+          return -1;
+      } else if (b.child_order < a.child_order) {
+          return 1;
+      }
+    return 0;
+  }
+
+
+  //if comparing ST and Child
+  //put ST first if ST section_order >= Child section_order
+  else if ((a.id && b.child_order) || (a.child_order && b.id)){
+    if (a.section_order < b.section_order) {
+        return -1;
+    } else if (b.section_order <= a.section_order) {
+        return 1;
+    }
+    return 0;
+  } else if (a.child_order && b.id) {
+    if (b.section_order < a.section_order) {
+        return -1;
+    }
+    if (a.section_order <= b.section_order) {
+        return 1;
+    }
+    return 0;
+  }
+
+  // a.key.split('-')[1]
+  // a.key.split('-')[2]
+
 }
 
 
