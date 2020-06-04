ce4a699878c411b262313c1c5258c969a95a2e6f
boom100100 <bsd231@nyu.edu>
2020-06-03 22:41:04 -0400

    Moved render to function.

--- frontend/src/components/EditProjectDataComponent.js
+++ frontend/src/components/EditProjectDataComponent.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import * as editHelpers from '../containers/helpers/EditProjectHelpers';
 
 const EditProjectDataComponent = (props) => {
+  const doRender = () => {
+    // console.log('EditProjectDataComponent props', props);
+    return document.getElementById('edit-project-parent') != null ? editHelpers.modifyElements(props.project) : undefined;
+  }
+
+  const doListen = () => {
+    props.state.addEventListener('change', doRender);
+  }
+  // let state = {values: doRender()}
+
+
   return (
     <div id='edit-project-data'>
+      {doRender()}
     </div>
   )
 }
