1f25fe5002e220764b18bd8b63dd6c231320770c
boom100100 <bsd231@nyu.edu>
2020-06-03 22:38:33 -0400

    Connected project from state to props.

--- frontend/src/components/EditProjectComponent.js
+++ frontend/src/components/EditProjectComponent.js
@@ -3,6 +3,7 @@ import EditProjectObjectComponent from './EditProjectObjectComponent';
 import EditProjectDataComponent from './EditProjectDataComponent';
 import EditProjectLinksComponent from './EditProjectLinksComponent';
 import EditProjectNewContainer from '../containers/EditProjectNewContainer';
+import { connect } from 'react-redux';
 
 const EditProjectComponent = (props) => {
 
@@ -12,13 +13,20 @@ const EditProjectComponent = (props) => {
       <button onClick={props.functions.onSave}>Save</button>
 
       <EditProjectObjectComponent state={props.state} />
-      <EditProjectDataComponent state={props.state} />
+      <EditProjectDataComponent state={props.state} project={props.project} />
       <EditProjectLinksComponent functions={props.functions} />
       <EditProjectNewContainer />
 
-      
+
     </div>
   )
 }
 
-export default EditProjectComponent;
+const mapStateToProps = (state) => {
+  let id = document.location.href.split('/').filter(x => x !== "").find(element => Number(element) >= 0);
+  return {
+    project: state.projects[id-1]
+  }
+}
+
+export default connect(mapStateToProps)(EditProjectComponent);
