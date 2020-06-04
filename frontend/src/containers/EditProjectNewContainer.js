8423bf8bc7f7dc493b39745081e188270301faf4
boom100100 <bsd231@nyu.edu>
2020-06-03 22:45:29 -0400

    Added support for preexisting data.

--- frontend/src/containers/EditProjectNewContainer.js
+++ frontend/src/containers/EditProjectNewContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import EditProjectNewSectionTitleContainer from '../containers/EditProjectNewSectionTitleContainer'
+import EditProjectNewSectionTitleContainer from './EditProjectNewSectionTitleContainer'
 import EditProjectNewResearchContainer from '../containers/EditProjectNewResearchContainer';
 import EditProjectNewGraphContainer from '../containers/EditProjectNewGraphContainer';
 
@@ -7,9 +7,9 @@ class EditProjectNewContainer extends Component {
   render(){
     return (
       <div id='add-new'>
-        <EditProjectNewSectionTitleContainer />
-        <EditProjectNewResearchContainer />
-        <EditProjectNewGraphContainer />
+        <EditProjectNewSectionTitleContainer id='add-new-section-title' />
+        <EditProjectNewResearchContainer id='add-new-research' parentId='' randomId='add-new-random-data' preliminaryId='add-new-preliminary-data' researchId='add-new-research-data' />
+        <EditProjectNewGraphContainer id='add-new-graph' />
       </div>
     )
   }
