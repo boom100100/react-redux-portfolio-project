d063710f0c9a64965205faf4d7bee300bb760528
boom100100 <bsd231@nyu.edu>
2020-06-15 09:33:36 -0400

    Passed down saveMethod prop.

--- frontend/src/containers/EditProjectNewContainer.js
+++ frontend/src/containers/EditProjectNewContainer.js
@@ -7,9 +7,9 @@ class EditProjectNewContainer extends Component {
   render(){
     return (
       <div id='add-new'>
-        <EditProjectNewSectionTitleContainer id='add-new-section-title' />
+        <EditProjectNewSectionTitleContainer saveMethod={"POST"} id='add-new-section-title' />
         <EditProjectNewResearchContainer id='add-new-research' parentId='' randomId='add-new-random-data' preliminaryId='add-new-preliminary-data' researchId='add-new-research-data' />
-        <EditProjectNewGraphContainer id='add-new-graph' />
+        <EditProjectNewGraphContainer saveMethod={"POST"} id='add-new-graph' />
       </div>
     )
   }
