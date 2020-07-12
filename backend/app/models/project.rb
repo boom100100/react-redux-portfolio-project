8260c3ed8ed9eccbc3ae357f2af54c79f7263128
boom100100 <bsd231@nyu.edu>
2020-07-12 17:27:18 -0400

    Linked section titles to project #destroy.

--- backend/app/models/project.rb
+++ backend/app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :user
 
-  has_many :section_titles
+  has_many :section_titles, dependent: :destroy
 
   has_many :data, through: :section_title
   has_many :graphs, through: :section_title
