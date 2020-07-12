a44084b2e0b11a51a7a091f8e8f9120efc301eed
boom100100 <bsd231@nyu.edu>
2020-07-12 17:40:39 -0400

    Removed unused code.

--- frontend/src/components/EditProjectDataComponent.js
+++ frontend/src/components/EditProjectDataComponent.js
@@ -3,16 +3,8 @@ import * as editHelpers from '../containers/helpers/EditProjectHelpers';
 
 const EditProjectDataComponent = (props) => {
   const doRender = () => {
-    // console.log('EditProjectDataComponent props', props);
-    // console.log('props.project', props.project);
     return document.getElementById('edit-project-parent') != null ? editHelpers.modifyElements(props.project) : undefined;
   }
-
-  // const doListen = () => {
-  //   props.state.addEventListener('change', doRender);
-  // }
-  // // let state = {values: doRender()}
-
 
   return (
     <div id='edit-project-data'>
