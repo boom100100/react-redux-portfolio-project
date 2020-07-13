d89abcca4733ef538677aa16425b448bc89aa4f7
boom100100 <bsd231@nyu.edu>
2020-07-13 16:26:47 -0400

    Fixed index bug.

--- frontend/src/reducers/projectsReducer.js
+++ frontend/src/reducers/projectsReducer.js
@@ -1,5 +1,5 @@
 function projectsReducer(state = [], action){
-  let newState, projectId, section_titles, section_order, section_id, child_order, allChildren;
+  let newState, projectId, section_titles, section_order, section_id, child_order, allChildren, index;
   switch(action.type){
     case 'ADD_PROJECTS':
       console.log('projects from reducer', action.projects);
@@ -30,16 +30,24 @@ function projectsReducer(state = [], action){
     case 'REPLACE_PROJECTS':
       return action.projects;
 
-    case 'NEW_PROJECT':
-      return [...state, action.projects];
+    case 'CREATE_PROJECT': //'EDIT_PROJECT', 'CREATE_PROJECT'
+      newState = [...state]
+      newState = newState.concat(action.project);
+      return newState;
 
     case 'DELETE_PROJECT':
-      return [...state];
+      console.log('action', action);
+      console.log('state', state);
+      newState = state.filter(x => x);
+      newState = newState.filter(x =>  x.id !== action.project.id);
+      // const filtered = newState.filter(element => element != null);
+      return newState;
 
     case 'ADD_SECTION':
 
       newState = [...state];
-      projectId = action.sectionTitle.project_id - 1;
+      projectId = action.sectionTitle.project_id;
+      index = newState.findIndex(x => x.id === projectId);
       let newSection = {
         id: action.sectionTitle.id,
         name: action.sectionTitle.name,
@@ -47,16 +55,16 @@ function projectsReducer(state = [], action){
         section_order: action.sectionTitle.section_order,
         section_title_children: []
       }
-      newState[projectId].section_titles.push(newSection);
+      newState[index].section_titles.push(newSection);
 
       //add 1 to section_order following sections
       //and conform children to have same section_order value
       //similar function also occurs in backend
 
-      newState[projectId].section_titles = levelUp(newState[projectId].section_titles, newSection, 'section_order');
+      newState[index].section_titles = levelUp(newState[index].section_titles, newSection, 'section_order');
 
       //sort sections
-      newState[projectId].section_titles = newState[projectId].section_titles.sort((a, b) => sortSection(a, b, 'section_order'));
+      newState[index].section_titles = newState[index].section_titles.sort((a, b) => sortSection(a, b, 'section_order'));
 
       return newState;
 
@@ -85,12 +93,13 @@ function projectsReducer(state = [], action){
       console.log('newData', newData, fields);
 
       //get section title
-      projectId = action.data.project_id - 1;
+      projectId = action.data.project_id;
+      index = newState.findIndex(x => x.id === projectId);
       section_id = action.data.section_title_child.section_title_id;
       section_order = action.data.section_order;
       child_order = action.data.child_order;
 
-      let titles = newState[projectId].section_titles
+      let titles = newState[index].section_titles
       let section_title_index = titles.findIndex(e => e.id === newData.section_title_id);
       console.log('fields', fields);
       const prev_section_id = fields.prev_section_id;
@@ -106,18 +115,18 @@ function projectsReducer(state = [], action){
       // console.log('newState[projectId].section_titles[section_title_index]', newState[projectId].section_titles[section_title_index]);
       // console.log('newState[projectId].section_titles[section_title_index].section_title_children', newState[projectId].section_titles[section_title_index].section_title_children);
       // console.log('json look for id of child to delete. will delete from before push.', action.data);
-      for (let section_title of newState[projectId].section_titles){
+      for (let section_title of newState[index].section_titles){
         section_title.section_title_children = section_title.section_title_children.filter(element => element.id !== action.data.delete_id);
       }
       // newState[projectId].section_titles[prev_section_id].section_title_children = newState[projectId].section_titles[prev_section_id].section_title_children.filter(element => element.id !== action.data.delete_id);
-      newState[projectId].section_titles[section_title_index].section_title_children.push(newData)
+      newState[index].section_titles[section_title_index].section_title_children.push(newData)
 
       //reorder following children
-      newState[projectId].section_titles[section_title_index].section_title_children = levelUp(newState[projectId].section_titles[section_title_index].section_title_children, newData, 'child_order');
+      newState[index].section_titles[section_title_index].section_title_children = levelUp(newState[index].section_titles[section_title_index].section_title_children, newData, 'child_order');
       // console.log('allChildren before sort', allChildren);
 
       //sort sections
-      newState[projectId].section_titles[section_title_index].section_title_children = newState[projectId].section_titles[section_title_index].section_title_children.sort((a, b) => sortSection(a, b, 'child_order'));
+      newState[index].section_titles[section_title_index].section_title_children = newState[index].section_titles[section_title_index].section_title_children.sort((a, b) => sortSection(a, b, 'child_order'));
       // console.log('allChildren after sort', allChildren);
       return newState;
 
