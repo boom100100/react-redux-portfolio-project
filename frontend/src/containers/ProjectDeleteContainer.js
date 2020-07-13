0fe1b5481d29697f244b623941eb9ab91bd2bff9
boom100100 <bsd231@nyu.edu>
2020-07-13 16:43:27 -0400

    Created, implemented project delete container.

---
+++ frontend/src/containers/ProjectDeleteContainer.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {deleteProject, deleteProjectState} from '../actions/ProjectActions';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+class ProjectDeleteContainer extends Component {
+  render(){ return (<><ProjectDeleteComponentConnected history={this.props.history} /></>); }
+}
+export default withRouter(ProjectDeleteContainer);
+
+const ProjectDeleteComponent = (props) => {
+  const doDelete = () => {
+    //remove from backend and frontend
+    //get project id, send in destroy method
+    //in backend:
+    //delete project, children (section titles, section title children)
+    //in frontend:
+    //simply delete the project from the store
+    //then, redirect
+    console.log('props.project', props);
+    // console.log('\'\' + props.project.id', '' + props.project.id);
+    // console.log('history split', props.history);
+    // props.history.location.split('/')[1]
+    console.log(props.projects);
+    props.deleteProject(props.project, '/projects/' + props.project.id, 'DELETE', props.deleteProjectState, props.history)
+
+
+    props.history.push('/projects');
+    props.history.go();
+  }
+
+  return (<div>{doDelete()}</div>);
+}
+
+const mapStateToProps = (state) => {
+  let id = document.location.href.split('/').filter(x => x !== "").find(element => Number(element) >= 0);
+  return {
+    project: state.projects.find(x => x.id == id),
+    projects: state.projects
+  }
+}
+
+const ProjectDeleteComponentConnected = connect(mapStateToProps, {deleteProject, deleteProjectState})(ProjectDeleteComponent);
