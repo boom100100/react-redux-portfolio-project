2a931e0400345bb02f3a2e1724b8d9780af9f5f5
boom100100 <bsd231@nyu.edu>
2020-07-14 18:44:22 -0400

    Moved creating components to here.

--- frontend/src/components/EditProjectDataComponent.js
+++ frontend/src/components/EditProjectDataComponent.js
@@ -1,16 +1,69 @@
-import React from 'react';
+import React, {Component} from 'react';
 import * as editHelpers from '../containers/helpers/EditProjectHelpers';
+import { connect } from 'react-redux';
 
-const EditProjectDataComponent = (props) => {
-  const doRender = () => {
-    return document.getElementById('edit-project-parent') != null ? editHelpers.modifyElements(props.project) : undefined;
+import EditProjectNewSectionTitleContainer from '../containers/EditProjectNewSectionTitleContainer';
+import EditProjectNewGraphContainer from '../containers/EditProjectNewGraphContainer';
+import NewRandomDataContainer from '../containers/NewRandomDataContainer';
+import NewPreliminaryDataContainer from '../containers/NewPreliminaryDataContainer';
+import NewResearchDataContainer from '../containers/NewResearchDataContainer';
+
+let keyCounter = 0;
+
+class EditProjectDataComponent extends Component {
+  state = {}
+  doRerender = () => {
+    this.setState({});
   }
 
-  return (
-    <div id='edit-project-data'>
-      {doRender()}
-    </div>
-  )
+  doRender = () => {
+    // return props.project ? editHelpers.modifyElements(props.project) : undefined;
+
+    let componentArray = []
+
+    for (let section of this.props.project.section_titles){
+      componentArray.push(section);
+      if (section.section_title_children)
+        for (let data of section.section_title_children){
+          data = {...data, section_order: section.section_order}
+          componentArray.push(data);
+        }
+    }
+    return componentArray.map(e => {
+
+      try {if (!isNaN(e.child_order)) {
+        if (e.type === 'Graph'){
+          keyCounter++;
+          e.divId = 'graph-' + e.section_title_id + '-' + e.child_order + '-' + keyCounter;
+          return (<EditProjectNewGraphContainer saveMethod={"PUT"} key={'graph-' + keyCounter} data={e} id={e.divId} />);
+        } else if (e.type === 'PreliminaryDatum'){
+          return (<NewPreliminaryDataContainer saveMethod={"PUT"} key={'edit-preliminary-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'preliminary-data-' + e.section_title_id + '-' + e.child_order} />);
+        } else if (e.type === 'ResearchDatum'){
+          return (<NewResearchDataContainer saveMethod={"PUT"} key={'edit-research-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'research-data-' + e.section_title_id + '-' + e.child_order} />);
+        } else if (e.type === 'RandomDatum'){
+          return (<NewRandomDataContainer saveMethod={"PUT"} key={'edit-random-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'random-data-' + e.section_title_id + '-' + e.child_order} />);
+        }
+      } else {
+        return (<EditProjectNewSectionTitleContainer saveMethod={"PUT"} key={'graph-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'section-title-' + e.section_order} />);
+      }} catch (error) {console.log(error);}
+    });
+  }
+  render(){
+    return (
+      <div id='edit-project-data'>
+        {this.doRender()}
+      </div>
+    )
+  }
 }
 
-export default EditProjectDataComponent;
+const mapStateToProps = (state) => {
+  let id = document.location.href.split('/').filter(x => x !== "").find(element => Number(element) >= 0);
+  const index = state.projects.findIndex(x => x.id == id);
+
+  return {
+    project: state.projects[index]
+  }
+}
+
+export default connect(mapStateToProps)(EditProjectDataComponent);
