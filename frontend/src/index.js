c7290556da9aa6c7af91aded1c8899f33ab35030
boom100100 <bsd231@nyu.edu>
2020-07-14 18:42:06 -0400

    Removed comments.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,20 +5,9 @@ import './index.css';
 import { store, persistor } from './reducers/configureStore';
 import { PersistGate } from 'redux-persist/integration/react'
 
-// import rootReducer from './reducers/rootReducer.js';
-//
-// import { createStore, applyMiddleware } from 'redux';
-// import thunk from 'redux-thunk';
-
-
 import { Provider } from 'react-redux';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-
-// console.log(store);
-// console.log(persistor);
-// sessionService.initSessionService(store);
-
 
 ReactDOM.render(
   <Provider store={store}>
