5bf51e2a5931be14755af646be04d54cb0cdb2f4
boom100100 <bsd231@nyu.edu>
2020-07-14 18:42:55 -0400

    Put code for creating components in other component. Fixed no-rerender issue.

--- frontend/src/containers/helpers/EditProjectHelpers.js
+++ frontend/src/containers/helpers/EditProjectHelpers.js
@@ -6,59 +6,58 @@ import NewPreliminaryDataContainer from '../NewPreliminaryDataContainer';
 import NewResearchDataContainer from '../NewResearchDataContainer';
 
 //manages displaying elements
-//and adding divs to show store and fetch data
 
-let keyCounter = 0;
-export const modifyElements = (project) => {
-  document.getElementById('edit-project-parent').firstElementChild.style.display = 'none';
-  let edit = document.getElementById('edit-project-data');
-  edit.innerHTML = '';
-  let componentArray = []
-  // console.log('project', project);
-
-  for (let section of project.section_titles){
-    // console.log('section', section);
-    componentArray.push(section);
-    // console.log('section.section_title_children', section.section_title_children);
-    if (section.section_title_children)
-      for (let data of section.section_title_children){
-        data = {...data, section_order: section.section_order}
-        // console.log('data', data);
-        componentArray.push(data)
-        // console.log('pushing data', data);
-      }
-  }
-
-  // componentArray.sort((a, b) => sorterSectionOrder(a, b)).sort((a, b) => sorterChildrenTitle(a, b));
-
-  return componentArray.map(e => {
-    // console.log('e', e);
-    try {if (!isNaN(e.child_order)) {
-      if (e.type === 'Graph'){
-        // console.log('is graph', e);
-        keyCounter++;
-        // const id = e.id;
-        // e.section_title_child_id = id;
-        e.divId = 'graph-' + e.section_title_id + '-' + e.child_order + '-' + keyCounter;
-        // console.log('is graph', e);
-        // console.log('order?', e.section_order);
-        return (<EditProjectNewGraphContainer saveMethod={"PUT"} key={'graph-' + keyCounter} data={e} id={e.divId} />);
-      } else if (e.type === 'PreliminaryDatum'){
-        // console.log('is prelim', e);
-        return (<NewPreliminaryDataContainer saveMethod={"PUT"} key={'edit-preliminary-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'preliminary-data-' + e.section_title_id + '-' + e.child_order} />);
-      } else if (e.type === 'ResearchDatum'){
-        // console.log('is research', e);
-        return (<NewResearchDataContainer saveMethod={"PUT"} key={'edit-research-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'research-data-' + e.section_title_id + '-' + e.child_order} />);
-      } else if (e.type === 'RandomDatum'){
-        // console.log('is random', e);
-        return (<NewRandomDataContainer saveMethod={"PUT"} key={'edit-random-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'random-data-' + e.section_title_id + '-' + e.child_order} />);
-      }
-    } else {
-      // console.log('is section title', e);
-      return (<EditProjectNewSectionTitleContainer saveMethod={"PUT"} key={'graph-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'section-title-' + e.section_order} />);
-    }} catch (error) {console.log(error);}
-  });
-}
+// let keyCounter = 0;
+// export const modifyElements = (project) => {
+//   // document.getElementById('edit-project-parent').firstElementChild.style.display = 'none';
+//   // let edit = document.getElementById('edit-project-data');
+//   // edit.innerHTML = '';
+//   let componentArray = []
+//   // console.log('project', project);
+//
+//   for (let section of project.section_titles){
+//     // console.log('section', section);
+//     componentArray.push(section);
+//     // console.log('section.section_title_children', section.section_title_children);
+//     if (section.section_title_children)
+//       for (let data of section.section_title_children){
+//         data = {...data, section_order: section.section_order}
+//         // console.log('data', data);
+//         componentArray.push(data)
+//         // console.log('pushing data', data);
+//       }
+//   }
+//
+//   // componentArray.sort((a, b) => sorterSectionOrder(a, b)).sort((a, b) => sorterChildrenTitle(a, b));
+//
+//   return componentArray.map(e => {
+//     // console.log('e', e);
+//     try {if (!isNaN(e.child_order)) {
+//       if (e.type === 'Graph'){
+//         // console.log('is graph', e);
+//         keyCounter++;
+//         // const id = e.id;
+//         // e.section_title_child_id = id;
+//         e.divId = 'graph-' + e.section_title_id + '-' + e.child_order + '-' + keyCounter;
+//         // console.log('is graph', e);
+//         // console.log('order?', e.section_order);
+//         return (<EditProjectNewGraphContainer saveMethod={"PUT"} key={'graph-' + keyCounter} data={e} id={e.divId} />);
+//       } else if (e.type === 'PreliminaryDatum'){
+//         // console.log('is prelim', e);
+//         return (<NewPreliminaryDataContainer saveMethod={"PUT"} key={'edit-preliminary-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'preliminary-data-' + e.section_title_id + '-' + e.child_order} />);
+//       } else if (e.type === 'ResearchDatum'){
+//         // console.log('is research', e);
+//         return (<NewResearchDataContainer saveMethod={"PUT"} key={'edit-research-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'research-data-' + e.section_title_id + '-' + e.child_order} />);
+//       } else if (e.type === 'RandomDatum'){
+//         // console.log('is random', e);
+//         return (<NewRandomDataContainer saveMethod={"PUT"} key={'edit-random-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'random-data-' + e.section_title_id + '-' + e.child_order} />);
+//       }
+//     } else {
+//       // console.log('e', e);
+//       return (<EditProjectNewSectionTitleContainer saveMethod={"PUT"} key={'graph-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'section-title-' + e.section_order} />);
+//     }} catch (error) {console.log(error);}
+//   });
+// }
 
 
 // const sorterChildrenTitle = (a, b) => {
