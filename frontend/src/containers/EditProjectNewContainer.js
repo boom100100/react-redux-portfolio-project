78c92c2d08c3449c4681c3f5b84e388e8dfb41a8
boom100100 <bsd231@nyu.edu>
2020-07-15 17:45:59 -0400

    Moved backend, state change functions to here.

--- frontend/src/containers/EditProjectNewContainer.js
+++ frontend/src/containers/EditProjectNewContainer.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import EditProjectNewSectionTitleContainer from './EditProjectNewSectionTitleContainer'
 import EditProjectNewResearchContainer from '../containers/EditProjectNewResearchContainer';
 import EditProjectNewGraphContainer from '../containers/EditProjectNewGraphContainer';
+import { addSectionToProject, addToBackend } from '../actions/ProjectActions';
+import { connect } from 'react-redux';
 
 class EditProjectNewContainer extends Component {
   render(){
     return (
       <div id='add-new'>
-        <EditProjectNewSectionTitleContainer saveMethod={"POST"} id='add-new-section-title' />
+        <EditProjectNewSectionTitleContainer myCall={this.props.addToBackend} myCallback={this.props.addSectionToProject} relativeUrl={"/section_titles"} saveMethod={"POST"} id='add-new-section-title' />
         <EditProjectNewResearchContainer id='add-new-research' parentId='' randomId='add-new-random-data' preliminaryId='add-new-preliminary-data' researchId='add-new-research-data' />
         <EditProjectNewGraphContainer saveMethod={"POST"} id='add-new-graph' />
       </div>
@@ -15,4 +17,4 @@ class EditProjectNewContainer extends Component {
   }
 }
 
-export default EditProjectNewContainer;
+export default connect(null, { addSectionToProject, addToBackend })(EditProjectNewContainer);
