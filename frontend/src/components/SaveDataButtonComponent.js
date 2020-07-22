92179bcfe90389e023f4af602fdd63d090494ac7
boom100100 <bsd231@nyu.edu>
2020-07-22 16:54:39 -0400

    Added props to data buttons.

--- frontend/src/components/SaveDataButtonComponent.js
+++ frontend/src/components/SaveDataButtonComponent.js
@@ -1,3 +1,9 @@
 import React from 'react';
-const SaveDataButtonComponent = (props) => {return(<><button onClick={e => props.click(e)}>Save Data</button></>)}
+const SaveDataButtonComponent = (props) => {
+  return(
+    <>
+      <button onClick={e => props.click(e)}>Save Data</button>
+      {props.deleteData ? <button onClick={e => props.deleteData(e)}>Delete Data</button> : undefined}
+    </>)
+}
 export default SaveDataButtonComponent;
