eb3fa386701c873be1b2ad162c27b5078f33a69e
boom100100 <bsd231@nyu.edu>
2020-07-26 17:01:11 -0400

    Abstracted thunk calls.

--- frontend/src/components/EditProjectNewResearchComponent.js
+++ frontend/src/components/EditProjectNewResearchComponent.js
@@ -2,15 +2,17 @@ import React from 'react';
 import NewRandomDataContainer from '../containers/NewRandomDataContainer';
 import NewPreliminaryDataContainer from '../containers/NewPreliminaryDataContainer';
 import NewResearchDataContainer from '../containers/NewResearchDataContainer';
+import { addDataToProject, addToBackend, updateSectionState, updateSection, updateDataState, updateData } from '../actions/ProjectActions';
+import { connect } from 'react-redux';
 
 const EditProjectNewResearchComponent = (props) => {
   return (
     <div id={props.parentId}>
-      <NewRandomDataContainer id={props.randomId} saveMethod={"POST"} />
-      <NewPreliminaryDataContainer id={props.preliminaryId} saveMethod={"POST"} />
-      <NewResearchDataContainer id={props.researchId} saveMethod={"POST"} />
+      <NewRandomDataContainer id={props.randomId} myCall={props.addToBackend} myCallback={props.addDataToProject} relativeUrl={"/section_title_children"} saveMethod={"POST"} />
+      <NewPreliminaryDataContainer id={props.preliminaryId} myCall={props.addToBackend} myCallback={props.addDataToProject} relativeUrl={"/section_title_children"} saveMethod={"POST"} />
+      <NewResearchDataContainer id={props.researchId} myCall={props.addToBackend} myCallback={props.addDataToProject} relativeUrl={"/section_title_children"} saveMethod={"POST"} />
     </div>
   )
 }
 
-export default EditProjectNewResearchComponent;
+export default connect(null, { addDataToProject, addToBackend, updateSectionState, updateSection, updateDataState, updateData })(EditProjectNewResearchComponent);
