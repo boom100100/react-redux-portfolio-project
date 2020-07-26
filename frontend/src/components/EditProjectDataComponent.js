13f3fae1e2c0624a6d06a312ceeff3fb3fc0f5ef
boom100100 <bsd231@nyu.edu>
2020-07-26 17:00:23 -0400

    Added data update actions. Fixed leveling.

--- frontend/src/components/EditProjectDataComponent.js
+++ frontend/src/components/EditProjectDataComponent.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import * as editHelpers from '../containers/helpers/EditProjectHelpers';
-import { updateSectionState, updateSection } from '../actions/ProjectActions';
+import { updateSectionState, updateSection, updateDataState, updateData } from '../actions/ProjectActions';
 import { connect } from 'react-redux';
 
 import EditProjectNewSectionTitleContainer from '../containers/EditProjectNewSectionTitleContainer';
@@ -15,35 +15,35 @@ class EditProjectDataComponent extends Component {
   state = {}
 
   doRender = () => {
-    // return props.project ? editHelpers.modifyElements(props.project) : undefined;
-
     let componentArray = []
-
+    // console.log('project', this.props.project);
     for (let section of this.props.project.section_titles){
       componentArray.push(section);
       if (section.section_title_children)
         for (let data of section.section_title_children){
+
           data = {...data, section_order: section.section_order}
           componentArray.push(data);
         }
     }
     return componentArray.map(e => {
-
+      // console.log('e', e);
       try {if (!isNaN(e.child_order)) {
         if (e.type === 'Graph'){
           keyCounter++;
           e.divId = 'graph-' + e.section_title_id + '-' + e.child_order + '-' + keyCounter;
-          return (<EditProjectNewGraphContainer saveMethod={"PUT"} key={'graph-' + keyCounter} data={e} id={e.divId} />);
+          return (<EditProjectNewGraphContainer myCall={this.props.updateData} myCallback={this.props.updateDataState} relativeUrl={`/section_title_children/${e.id}`} saveMethod={"PUT"} key={'graph-' + keyCounter} data={e} id={e.divId} />);
         } else if (e.type === 'PreliminaryDatum'){
-          return (<NewPreliminaryDataContainer saveMethod={"PUT"} key={'edit-preliminary-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'preliminary-data-' + e.section_title_id + '-' + e.child_order} />);
+          return (<NewPreliminaryDataContainer myCall={this.props.updateData} myCallback={this.props.updateDataState} relativeUrl={`/section_title_children/${e.id}`} saveMethod={"PUT"} key={'edit-preliminary-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'preliminary-data-' + e.section_title_id + '-' + e.child_order} />);
         } else if (e.type === 'ResearchDatum'){
-          return (<NewResearchDataContainer saveMethod={"PUT"} key={'edit-research-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'research-data-' + e.section_title_id + '-' + e.child_order} />);
+          return (<NewResearchDataContainer myCall={this.props.updateData} myCallback={this.props.updateDataState} relativeUrl={`/section_title_children/${e.id}`} saveMethod={"PUT"} key={'edit-research-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'research-data-' + e.section_title_id + '-' + e.child_order} />);
         } else if (e.type === 'RandomDatum'){
-          return (<NewRandomDataContainer saveMethod={"PUT"} key={'edit-random-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'random-data-' + e.section_title_id + '-' + e.child_order} />);
+          return (<NewRandomDataContainer myCall={this.props.updateData} myCallback={this.props.updateDataState} relativeUrl={`/section_title_children/${e.id}`} saveMethod={"PUT"} key={'edit-random-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'random-data-' + e.section_title_id + '-' + e.child_order} />);
         }
       } else {
         return (<EditProjectNewSectionTitleContainer myCall={this.props.updateSection} myCallback={this.props.updateSectionState} relativeUrl={`/section_titles/${e.id}`} saveMethod={"PUT"} key={'graph-' + e.section_order + '-' + e.child_order + '-' + ++keyCounter} data={e} id={'section-title-' + e.section_order} />);
       }} catch (error) {console.log(error);}
+      return undefined;
     });
   }
   render(){
@@ -64,4 +64,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps, { updateSectionState, updateSection })(EditProjectDataComponent);
+export default connect(mapStateToProps, { updateSectionState, updateSection, updateDataState, updateData })(EditProjectDataComponent);
