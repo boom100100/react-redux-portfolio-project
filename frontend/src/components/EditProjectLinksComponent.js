f4c0a074940fcaa3b52feee3d3dc5c911ea4c065
boom100100 <bsd231@nyu.edu>
2020-07-26 17:00:41 -0400

    Implemented bootstrap.

--- frontend/src/components/EditProjectLinksComponent.js
+++ frontend/src/components/EditProjectLinksComponent.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 const EditProjectLinksComponent = (props) => {
   return (
     <div>
       <div id='primary-links'>
-          <button onClick={props.functions.addSectionTitle}>Add Section Title</button>
-          <button onClick={props.functions.showAddDataOptions}>Add Research</button>
-          <button onClick={props.functions.addGraph}>Add Graph</button>
+          <Button variant="primary" onClick={props.functions.addSectionTitle}>Add Section Title</Button>
+          <Button variant="primary" onClick={props.functions.showAddDataOptions}>Add Research</Button>
+          <Button variant="primary" onClick={props.functions.addGraph}>Add Graph</Button>
       </div>
 
       <div id='secondary-links' >
-          <button onClick={props.functions.addPreliminaryData}>Add Preliminary Data</button>
-          <button onClick={props.functions.addResearchData}>Add Research Data</button>
-          <button onClick={props.functions.addRandomData}>Add Random Data</button>
-          <button onClick={props.functions.showPrimaryOptions}>Back</button>
+          <Button variant="primary" onClick={props.functions.addPreliminaryData}>Add Preliminary Data</Button>
+          <Button variant="primary" onClick={props.functions.addResearchData}>Add Research Data</Button>
+          <Button variant="primary" onClick={props.functions.addRandomData}>Add Random Data</Button>
+          <Button variant="info" onClick={props.functions.showPrimaryOptions}>Back</Button>
       </div>
     </div>
 
