3e5fa5de026464ca2e142e0e7d8a6f53d8d51353
boom100100 <bsd231@nyu.edu>
2020-07-26 16:59:54 -0400

    Removed unnecessary code.

--- frontend/src/components/EditProjectComponent.js
+++ frontend/src/components/EditProjectComponent.js
@@ -5,13 +5,11 @@ import EditProjectLinksComponent from './EditProjectLinksComponent';
 import EditProjectNewContainer from '../containers/EditProjectNewContainer';
 import { connect } from 'react-redux';
 
+
 const EditProjectComponent = (props) => {
 
   return (
     <div id='edit-project-parent'>
-      
-      <button onClick={e => props.functions.onSave(e)}>Save</button>
-
       <EditProjectObjectComponent myState={props.myState} saveProject={props.functions.saveProject} callbackType={'EDIT_PROJECT'} />
       <EditProjectDataComponent />
       <EditProjectLinksComponent functions={props.functions} />
