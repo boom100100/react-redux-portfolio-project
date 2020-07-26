71c9687881c8491abe56144f048c174dd0e4ef3a
boom100100 <bsd231@nyu.edu>
2020-07-26 17:10:06 -0400

    Added to let declarations.

--- frontend/src/reducers/projectsReducer.js
+++ frontend/src/reducers/projectsReducer.js
@@ -1,5 +1,5 @@
 function projectsReducer(state = [], action){
-  let newState, projectId, section_titles, section_order, section_id, child_order, allChildren, index, sectionIndex, editedData;
+  let newState, projectId, section_titles, section_order, section_id, child_order, allChildren, index, sectionIndex, editedData, fields, searchRegExp, replaceWith, newData, titles, section_title_index, prev_section_id, title;
   switch(action.type){
     case 'ADD_PROJECTS':
       action.projects.forEach(e => {
@@ -106,11 +106,17 @@ function projectsReducer(state = [], action){
           section_titles.push(JSON.parse(JSON.stringify(e)));
         }
 
+        // return highest section_order
+        // later, if action.sectionTitle.section_order is higher, use max variable for new section order instead
+        // const max = section_titles.reduce(function(a, b){console.log('a.section_order', a.section_order);return Math.max(a.section_order, b.section_order)});
+        // bug: must also subtract one from last or indexes can have a +1 error
+
         section_titles = levelDown(section_titles, section_titles[sectionIndex], 'section_order');
 
         // push
         // reorder the edited data
         // then, readd the edited data in new order
+        // action.sectionTitle.section_order > max ? section_titles[sectionIndex].section_order = max : section_titles[sectionIndex].section_order = action.sectionTitle.section_order;
         section_titles[sectionIndex].section_order = action.sectionTitle.section_order;
 
         // level up
@@ -154,7 +160,7 @@ function projectsReducer(state = [], action){
 
       //need to do deep clone of array's objects!
       //otherwise, state changes and won't rerender
-      // ADD_SECTION has same requirement
+      // ADD_SECTION and EDIT_DATA have same requirement
       newState = [];
       for (let e of state){
         //JSON-serializable content only
@@ -162,13 +168,13 @@ function projectsReducer(state = [], action){
         newState.push(JSON.parse(JSON.stringify(e)));
       }
       //extract child object
-      let fields = (action.data.section_title_child || action.data);
+      fields = (action.data.section_title_child || action.data);
 
-      const searchRegExp = /=>/g;
-      const replaceWith = ':';
+      searchRegExp = /=>/g;
+      replaceWith = ':';
       if (action.data.type === 'Graph')
         fields.content = ({graph: JSON.parse(fields.content.replace(searchRegExp, replaceWith))});
-      const newData = {
+      newData = {
         id: fields.id,
         project_id: action.data.project_id,
         section_title_id: fields.section_title_id,
@@ -183,20 +189,16 @@ function projectsReducer(state = [], action){
       //get section title
       projectId = action.data.project_id;
       index = newState.findIndex(x => x.id === projectId);
-      console.log('action.data', action.data);
       section_id = action.data.section_title_child.section_title_id;
       section_order = action.data.section_order;
       child_order = action.data.child_order;
 
-      let titles = newState[index].section_titles
-      let section_title_index = titles.findIndex(e => e.id === newData.section_title_id);
-      const prev_section_id = fields.prev_section_id;
-      let title = titles[section_title_index]
+      titles = newState[index].section_titles
+      section_title_index = titles.findIndex(e => e.id === newData.section_title_id);
+      prev_section_id = fields.prev_section_id;
+      title = titles[section_title_index]
       // console.log('title', title);
       // title.section_title_children.push(newData)
-
-      // console.log('newState', newState);
-
 
       // //add 1 to section_order equal and following sections
       //push to section title
@@ -218,20 +220,81 @@ function projectsReducer(state = [], action){
     case 'EDIT_DATA':
       newState = [];
       for (let e of state){
+        //JSON-serializable content only
+        // (no functions, no Number.POSITIVE_INFINITY
         newState.push(JSON.parse(JSON.stringify(e)));
       }
-      projectId = action.sectionTitle.project_id;
+      //extract child object
+      fields = (action.data.section_title_child || action.data);
+
+      searchRegExp = /=>/g;
+      replaceWith = ':';
+      if (action.data.type === 'Graph')
+        fields.content = ({graph: JSON.parse(fields.content.replace(searchRegExp, replaceWith))});
+      newData = {
+        id: fields.id,
+        project_id: action.data.project_id,
+        section_title_id: fields.section_title_id,
+        name: fields.name,
+        child_order: fields.child_order,
+        description:  fields.description,
+        content: fields.content,
+        type: action.data.type,
+        url: fields.url,
+      }
+
+
+      //get section title
+      projectId = action.data.project_id;
       index = newState.findIndex(x => x.id === projectId);
-      sectionIndex = newState[index].section_titles.findIndex(x => x.id === action.sectionTitle.id);
+      section_id = action.data.section_title_child.section_title_id;
+      section_order = action.data.section_order;
+      child_order = action.data.child_order;
 
-      newState[index].section_titles[sectionIndex].name = action.sectionTitle.name;
-      newState[index].section_titles[sectionIndex].section_order = action.sectionTitle.section_order;
+      titles = newState[index].section_titles
+      section_title_index = titles.findIndex(e => e.id === newData.section_title_id);
+      prev_section_id = fields.prev_section_id;
+      title = titles[section_title_index]
+      // console.log('title', title);
+      // title.section_title_children.push(newData)
 
-      //and conform children to have same section_order value
-      //similar function also occurs in backend
-      newState[index].section_titles = levelUp(newState[index].section_titles, newState[index].section_titles[sectionIndex], 'section_order');
-      newState[index].section_titles = newState[index].section_titles.sort((a, b) => sortSection(a, b, 'section_order'));
+      // //add 1 to section_order equal and following sections
+      //push to section title
+      let prevChildOrder;
+      for (let section_title of newState[index].section_titles){
+        section_title.section_title_children = section_title.section_title_children.filter(element => {
+          if (element.id === action.data.delete_id){
+            prevChildOrder = element.child_order;
+          }
+            // console.log('prevChildOrder', prevChildOrder);
+          return element.id !== action.data.delete_id;
+        });
+      }
+      // newState[projectId].section_titles[prev_section_id].section_title_children = newState[projectId].section_titles[prev_section_id].section_title_children.filter(element => element.id !== action.data.delete_id);
+      // console.log('newData', newData);
+      // console.log('before newState[index].section_titles[section_title_index].section_title_children', newState[index].section_titles[section_title_index].section_title_children);
+      newState[index].section_titles[section_title_index].section_title_children.push(newData);
 
+      // TODO:
+      // graph wouldn't edit in good order
+      // child_order figures mess up
+      // Moving data between sections is not smooth
+      // prev doesn't update
+      //
+
+      //reorder following children
+      // if moving down
+      if (action.movingDown){
+        newState[index].section_titles[section_title_index].section_title_children = levelLowers(newState[index].section_titles[section_title_index].section_title_children, newData, 'child_order', prevChildOrder);
+      } else {
+        newState[index].section_titles[section_title_index].section_title_children = levelUpAlt(newState[index].section_titles[section_title_index].section_title_children, newData, 'child_order', prevChildOrder);
+      }
+      // console.log('allChildren before sort', allChildren);
+
+      //sort sections
+      newState[index].section_titles[section_title_index].section_title_children = newState[index].section_titles[section_title_index].section_title_children.sort((a, b) => sortSection(a, b, 'child_order'));
+      // console.log('allChildren after sort', allChildren);
+      // console.log('after newState[index].section_titles[section_title_index].section_title_children', newState[index].section_titles[section_title_index].section_title_children);
       return newState;
 
     case 'DELETE_DATA':
@@ -266,9 +329,22 @@ function projectsReducer(state = [], action){
 }
 
 //array, elementArg, key
-const levelUp = (array, elementArg, key) => {
+const levelUp = (array, elementArg, key, prevChildOrder) => {
   return array.map(e => {
+    // if (prevChildOrder > e[key] && e[key] >= elementArg[key] && elementArg !== e){
     if (e[key] >= elementArg[key] && elementArg !== e){
+      e[key]++
+      if (key === 'section_order')
+        e.section_title_children.forEach(child => child.section_order++ );
+    }
+    return e;
+  });
+}
+
+const levelUpAlt = (array, elementArg, key, prevChildOrder) => {
+  return array.map(e => {
+    if (prevChildOrder > e[key] && e[key] >= elementArg[key] && elementArg !== e){
+    // if (e[key] >= elementArg[key] && elementArg !== e){
       e[key]++
       if (key === 'section_order')
         e.section_title_children.forEach(child => child.section_order++ );
@@ -283,6 +359,15 @@ const levelDown = (array, elementArg, key) => {
       e[key]--
       if (key === 'section_order')
         e.section_title_children.forEach(child => child.section_order-- );
+    }
+    return e;
+  });
+}
+
+const levelLowers = (array, elementArg, key, prevChildOrder) => {
+  return array.map(e => {
+    if (prevChildOrder < e[key] && e[key] <= elementArg[key] && elementArg !== e){
+      e[key]--
     }
     return e;
   });
