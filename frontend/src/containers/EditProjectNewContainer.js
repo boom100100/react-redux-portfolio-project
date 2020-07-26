b94147ac4f2565a3f1ed53b52cfa7cfaba0e76dc
boom100100 <bsd231@nyu.edu>
2020-07-26 17:02:26 -0400

    Fixed leveling.

--- frontend/src/containers/EditProjectNewContainer.js
+++ frontend/src/containers/EditProjectNewContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import EditProjectNewSectionTitleContainer from './EditProjectNewSectionTitleContainer'
 import EditProjectNewResearchContainer from '../containers/EditProjectNewResearchContainer';
 import EditProjectNewGraphContainer from '../containers/EditProjectNewGraphContainer';
-import { addSectionToProject, addToBackend } from '../actions/ProjectActions';
+import { addSectionToProject, addToBackend, addDataToProject } from '../actions/ProjectActions';
 import { connect } from 'react-redux';
 
 class EditProjectNewContainer extends Component {
@@ -11,10 +11,10 @@ class EditProjectNewContainer extends Component {
       <div id='add-new'>
         <EditProjectNewSectionTitleContainer myCall={this.props.addToBackend} myCallback={this.props.addSectionToProject} relativeUrl={"/section_titles"} saveMethod={"POST"} id='add-new-section-title' />
         <EditProjectNewResearchContainer id='add-new-research' parentId='' randomId='add-new-random-data' preliminaryId='add-new-preliminary-data' researchId='add-new-research-data' />
-        <EditProjectNewGraphContainer saveMethod={"POST"} id='add-new-graph' />
+        <EditProjectNewGraphContainer myCall={this.props.addToBackend} myCallback={this.props.addDataToProject} relativeUrl={"/section_title_children"} saveMethod={"POST"} id='add-new-graph' />
       </div>
     )
   }
 }
 
-export default connect(null, { addSectionToProject, addToBackend })(EditProjectNewContainer);
+export default connect(null, { addSectionToProject, addToBackend, addDataToProject })(EditProjectNewContainer);
